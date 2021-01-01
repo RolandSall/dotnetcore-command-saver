f2a55a640e026b2484c9f24fb42e8cd34d440f89
Roland Salloum <roland.salloum00@outlook.com>
2021-01-01 17:34:36 +0200

    feat: added controller to test dummy API

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using dotnetcore_command_saver.Models;
+using dotnetcore_command_saver.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetcore_command_saver.Controllers {
+    
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+
+        private readonly CommandRepository _repository = new CommandRepository();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            return Ok(_repository.GetAppCommands());
+        }
+        
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id) {
+            return Ok(_repository.GetCommandById(id));  
+        }
+    }
+}
+
