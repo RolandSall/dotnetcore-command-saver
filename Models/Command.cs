019cec0d212ebaf282d6caa431114a47d4284638
Roland Salloum <roland.salloum00@outlook.com>
2021-01-01 16:45:28 +0200

    fix: model class definition

--- Models/Command.cs
+++ Models/Command.cs
@@ -1,9 +1,20 @@
 ﻿namespace dotnetcore_command_saver.Models{
     public class Command {
+        
         public int Id { get; set; }
+
         public string Objective { get; set; }
+
         public string Line { get; set; }
+
         public string Platform { get; set; }
         
+        public Command(int id, string objective, string line, string platform)
+        {
+            Id = id;
+            Objective = objective;
+            Line = line;
+            Platform = platform;
+        }
     }
 }
---
+++ Repository/CommandRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using dotnetcore_command_saver.Models;
+
+namespace dotnetcore_command_saver.Repository {
+    
+    public class CommandRepository : ICommandRepository{
+        public IEnumerable<Command> GetAppCommands() {
+            var cmd = new Command(0,"Desc", "Line", "Platform");
+            var commands = new List<Command> {cmd, cmd};
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            var cmd = new Command(0,"Desc", "Line", "Platform");
+            return cmd;
+        }
+    }
+}
