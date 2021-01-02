2058e1c352c9261c7840fbc2dd4d35afd75e5e8d
Roland Salloum <roland.salloum00@outlook.com>
2021-01-03 00:54:14 +0200

    fix: return detailed response error on bad post requests

--- DTOs/CommandCreateDto.cs
+++ DTOs/CommandCreateDto.cs
@@ -1,11 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace dotnetcore_command_saver.DTOs {
     
     public class CommandCreateDto {
 
+        [Required]
+        [MaxLength(150)]
         public string Objective { get; set; }
        
+        [Required]
         public string Line { get; set; }
 
+        [Required]
         public string Platform { get; set; }
         
         public CommandCreateDto(string objective, string line, string platform) {
